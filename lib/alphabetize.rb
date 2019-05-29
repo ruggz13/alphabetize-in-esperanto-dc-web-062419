4849fe39e34b075015bd997aecb324d5a7e2b145
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-29 16:34:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   # code here
-  arr.sort_by{ |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+  arr.sort_by{ |word| word.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
